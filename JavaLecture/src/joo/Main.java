bc99c86a22b6b4f426b95f8ada05aa344523b52a
주병 박 <zest132@naver.com>
2023-01-08 05:13:19 +0900

    2강 마무리

--- JavaLecture/src/joo/Main.java
+++ JavaLecture/src/joo/Main.java
@@ -1,30 +1,25 @@
 package joo;
 
-import java.util.Date;
+import java.util.Scanner;
 
-//자바를 처음시작하는 예제
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
-		
-		float test = 13;
-		
-		
-		 char a = 110;
-		 
-		 int b= 'a';
-		 
-		 int c = (int)3.14;
+	/*
+	char ch;
+	Scanner scanner = new Scanner(System.in); //입력을 받기 위한 스캐너 객체를 생성
 	
-		 double d = 3.14f;
-		 
-		 
-		 System.out.println(b);
-		
-		
-		
+	System.out.println("값을 입력하세요:");
+	String input = scanner.nextLine(); //키보드로부터 값을 입력 받아 input 변수에 저장한다.
+	ch = input.charAt(0);  //input 변수의 타입을 char로 변경한다.
+	*/
+	
+	int f = 100;
+	float c = (int)(5f/9 * (f-32)*10+0.5)/10f;
+
+	System.out.println("화씨 : "+f );
+	System.out.println("섭씨 : "+c );
 
 	}
 
