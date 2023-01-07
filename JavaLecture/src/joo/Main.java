abe57809a326bb88a9a54b6ff5910d3aa747f990
주병 박 <zest132@naver.com>
2023-01-07 13:10:48 +0900

    1강 강의자료 완료

--- JavaLecture/src/joo/Main.java
+++ JavaLecture/src/joo/Main.java
@@ -1,11 +1,30 @@
 package joo;
 
+import java.util.Date;
+
+//자바를 처음시작하는 예제
 public class Main {
 
 	public static void main(String[] args) {
-
-System.out.println("test");
-
+		
+		
+		
+		float test = 13;
+		
+		
+		 char a = 110;
+		 
+		 int b= 'a';
+		 
+		 int c = (int)3.14;
+	
+		 double d = 3.14f;
+		 
+		 
+		 System.out.println(b);
+		
+		
+		
 
 	}
 
