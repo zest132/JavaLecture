a3b711a787d03d558cb13a82fa2181b1e3a513fb
주병 박 <zest132@naver.com>
2023-01-11 20:49:11 +0900

    2강 요약 완료

--- JavaLecture/src/joo/Main.java
+++ JavaLecture/src/joo/Main.java
@@ -6,20 +6,24 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-	/*
+	
 	char ch;
 	Scanner scanner = new Scanner(System.in); //입력을 받기 위한 스캐너 객체를 생성
 	
-	System.out.println("값을 입력하세요:");
+	System.out.println("성적을 입력하세요:");
 	String input = scanner.nextLine(); //키보드로부터 값을 입력 받아 input 변수에 저장한다.
-	ch = input.charAt(0);  //input 변수의 타입을 char로 변경한다.
-	*/
 	
-	int f = 100;
-	float c = (int)(5f/9 * (f-32)*10+0.5)/10f;
+	int score = Integer.parseInt(input); // string 타입의 변수를 int로 변경한다.
+	
 
-	System.out.println("화씨 : "+f );
-	System.out.println("섭씨 : "+c );
+	if(score>=60) 
+		System.out.println("합격입니다.");
+	
+	if(score<60) 
+		System.out.println("불합격입니다.");
+
+	
+
 
 	}
 
