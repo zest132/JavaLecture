29689c62728ae635b0f14b581ae8d89d1e5ee756
주병 박 <zest132@naver.com>
2023-01-12 17:04:07 +0900

    복습 문제 작성중

--- JavaLecture/src/joo/Main.java
+++ JavaLecture/src/joo/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-	
+	/*
 	char ch;
 	Scanner scanner = new Scanner(System.in); //입력을 받기 위한 스캐너 객체를 생성
 	
@@ -22,8 +22,16 @@ public class Main {
 	if(score<60) 
 		System.out.println("불합격입니다.");
 
+	*/
+		
+	int radious= 13;
+	final float PI = 3.14f;
 	
-
+	//System.out.println(radious*2* PI);
+	double d = 5.81f;
+	int I = 3.14;
+	System.out.println((int)'1');
+	System.out.println('1' + '2');
 
 	}
 
