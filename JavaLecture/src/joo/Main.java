b9a6c8348a514c87342c9d90f518201b2603af87
주병 박 <zest132@naver.com>
2023-01-13 02:09:02 +0900

    복습문제 완료

--- JavaLecture/src/joo/Main.java
+++ JavaLecture/src/joo/Main.java
@@ -24,15 +24,34 @@ public class Main {
 
 	*/
 		
-	int radious= 13;
-	final float PI = 3.14f;
-	
-	//System.out.println(radious*2* PI);
-	double d = 5.81f;
-	int I = 3.14;
-	System.out.println((int)'1');
-	System.out.println('1' + '2');
-
+		문제9();
+		
 	}
+	
 
+	
+	public static void 문제9()
+	{
+		int x = 2;
+		int y = 7;
+		char c = 'A'; 
+		System.out.println(1 + x < 3);
+		System.out.println(++x);
+		System.out.println(x++);
+		System.out.println(y >= 7 || x > 0 && x <= 3);
+		System.out.println(x+=2);
+		System.out.println( !('A' <= c && c <='Z') );
+		System.out.println('C'-c);
+		System.out.println('8'-'0');
+		System.out.println(x);
+	}
+	
+	public static  void 문제10()
+	{
+		char ch = '3';
+		
+		boolean result = (ch-'0') >=0 && ('9'-ch) <=9; 
+		
+		System.out.println(result);
+	}
 }
