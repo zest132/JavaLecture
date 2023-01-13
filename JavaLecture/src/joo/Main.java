036e3ec45c19751f698575ce7e89e408c96afdc0
주병 박 <zest132@naver.com>
2023-01-14 03:15:46 +0900

    3강 작업중

--- JavaLecture/src/joo/Main.java
+++ JavaLecture/src/joo/Main.java
@@ -1,42 +1,24 @@
 package joo;
 
-import java.util.Scanner;
 
 
 public class Main {
 
 	public static void main(String[] args) {
 	
-	char ch;
-	Scanner scanner = new Scanner(System.in); //입력을 받기 위한 스캐너 객체를 생성
-	
-	System.out.println("성적을 입력하세요");
-	String input = scanner.nextLine(); //키보드로부터 값을 입력 받아 input 변수에 저장한다.
-	
-	int score = Integer.parseInt(input); // string 타입의 변수를 int로 변경한다.
-	
-	int level = 1;
-	
-	switch(level)
-	{
-	case 3:
 		
-	}
+		
+		강의3실습문제 temp = new 강의3실습문제();
+		
+		temp.최종실습_1();
 
+		
+		
+		
+		
+	
+		
 
-	
-	
-	
-	/*
-	if(score<60) 
-		System.out.println("불합격입니다.");
-
-	*/
-	
-	
-		
-		
-		
 	}
 	
 
---
+++ JavaLecture/src/joo/Util.java
@@ -0,0 +1,32 @@
+package joo;
+
+import java.util.Scanner;
+
+public class  Util {
+
+	
+	
+	public static String getKeyBoardValue(String str)
+	{
+		
+		Scanner scanner = new Scanner(System.in); //입력을 받기 위한 스캐너 객체를 생성
+		
+		System.out.println(str);
+		String input = scanner.nextLine(); //키보드로부터 값을 입력 받아 input 변수에 저장한다.
+		
+		return input;
+
+	}
+	
+	public static int getKeyBoardValueInt(String str)
+	{
+		return Integer.parseInt(getKeyBoardValue(str)); 
+	}
+	
+	public static void getRandom()
+	{
+		
+		
+		System.out.println(Math.random());
+	}
+}
