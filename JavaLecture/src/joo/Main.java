d8b0d4c21d6e52322a576c540f5ac8b465c10c26
주병 박 <zest132@naver.com>
2023-01-13 22:10:42 +0900

    3강 작업중

--- JavaLecture/src/joo/Main.java
+++ JavaLecture/src/joo/Main.java
@@ -6,52 +6,40 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-	/*
+	
 	char ch;
 	Scanner scanner = new Scanner(System.in); //입력을 받기 위한 스캐너 객체를 생성
 	
-	System.out.println("성적을 입력하세요:");
+	System.out.println("성적을 입력하세요");
 	String input = scanner.nextLine(); //키보드로부터 값을 입력 받아 input 변수에 저장한다.
 	
 	int score = Integer.parseInt(input); // string 타입의 변수를 int로 변경한다.
 	
-
-	if(score>=60) 
-		System.out.println("합격입니다.");
+	int level = 1;
 	
+	switch(level)
+	{
+	case 3:
+		
+	}
+
+
+	
+	
+	
+	/*
 	if(score<60) 
 		System.out.println("불합격입니다.");
 
 	*/
+	
+	
 		
-		문제9();
+		
 		
 	}
 	
 
 	
-	public static void 문제9()
-	{
-		int x = 2;
-		int y = 7;
-		char c = 'A'; 
-		System.out.println(1 + x < 3);
-		System.out.println(++x);
-		System.out.println(x++);
-		System.out.println(y >= 7 || x > 0 && x <= 3);
-		System.out.println(x+=2);
-		System.out.println( !('A' <= c && c <='Z') );
-		System.out.println('C'-c);
-		System.out.println('8'-'0');
-		System.out.println(x);
-	}
 	
-	public static  void 문제10()
-	{
-		char ch = '3';
-		
-		boolean result = (ch-'0') >=0 && ('9'-ch) <=9; 
-		
-		System.out.println(result);
-	}
 }
