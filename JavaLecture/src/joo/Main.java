9c8c21380d31ef7d3f9d1492a22f15e192e9587b
주병 박 <zest132@naver.com>
2023-01-27 00:43:54 +0900

    5강 작업중

--- JavaLecture/src/joo/Main.java
+++ JavaLecture/src/joo/Main.java
@@ -1,24 +1,23 @@
 package joo;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 	
 		
-		int[] student= {30,50};
+		char[] str = new char[50];
 		
-		System.out.println(student.toString());
 		
-		System.out.println(Arrays.toString(student));
-
+		str[0] = 'k';
+		str[1] = 'o';
+		str[2] = 'r';
+		str[3] = 'e';
+		str[4] = 'a';
 		
-		for(int i =0 ; i<student.length;i++)
-			System.out.println(student[i]);
 		
-	
+		System.out.println(str);
 	}
 	
 
