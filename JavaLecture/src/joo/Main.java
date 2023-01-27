9534a7df93415c55e6df6fd66186fa05ecccb3f1
주병 박 <zest132@naver.com>
2023-01-27 17:00:27 +0900

    5강 자료 만드는중

--- JavaLecture/src/joo/Main.java
+++ JavaLecture/src/joo/Main.java
@@ -7,17 +7,21 @@ public class Main {
 	public static void main(String[] args) {
 	
 		
-		char[] str = new char[50];
+		int[] arr = {10,20,30,40,50};
+		
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+		
+		int sum=0;
+		
+		for(int i = 0 ;i<arr.length; i++)
+			sum +=arr[i];
 		
 		
-		str[0] = 'k';
-		str[1] = 'o';
-		str[2] = 'r';
-		str[3] = 'e';
-		str[4] = 'a';
-		
-		
-		System.out.println(str);
+		System.out.println(sum/(float)arr.length);
 	}
 	
 
