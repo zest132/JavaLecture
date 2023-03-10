d8aca6815d32b93757b9ac4d95f1560402965d5d
주병 박 <zest132@naver.com>
2023-03-10 17:50:46 +0900

    12강  작업중

--- JavaLecture/src/joo/twelve/Main.java
+++ JavaLecture/src/joo/twelve/Main.java
@@ -1,19 +1,42 @@
 package joo.twelve;
 
-import joo.Student;
-import joo.nine.OilCar;
-
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		try
+		{
+			int a =10/0;
+			
+			try
+			{
+				System.out.println("try catch 중첩 사용가능");
+			}catch(Exception ex)
+			{
+				
+			}
+			
+		}catch(NullPointerException ex)
+		{
+			try
+			{
+				System.out.println("try catch 중첩 사용가능");
+			}catch(Exception ex)
+			{
+				
+			}
+			System.out.println("객체를 생성하고 쓰세요");
+		}
+		catch(ArithmeticException ex)
+		{
+			System.out.println("0으로 나누지 마세요 ㅡㅡ");
+		}
+			
+	
 		
-		Student[] test = (Student)new OilCar();
 		
-		
-		test[2].name = "학생1";
+
 		
 		
 	}
