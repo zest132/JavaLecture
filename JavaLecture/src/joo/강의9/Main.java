c6b00ef6d77df7c6ad28f2e40cf159f0a8d92b98
주병 박 <zest132@naver.com>
2023-04-22 13:43:32 +0900

    9강 마무리

--- JavaLecture/src/joo/강의9/Main.java
+++ JavaLecture/src/joo/강의9/Main.java
@@ -1,5 +1,39 @@
 package joo.강의9;
 
+import java.awt.Point;
+
+
+class Shape 
+{
+	Point p;
+	
+	Shape() 
+	{
+		this(new Point(0,0));
+	}
+	
+	Shape(Point p) 
+	{
+		this.p = p;
+	}
+	
+	// 도형의 면적을 계산해서 반환하는 메서드
+	double calcArea()
+	{
+		return 1;
+	}
+	
+	Point getPosition() 
+	{
+		return p;
+	}
+	
+	void setPosition(Point p) 
+	{
+		this.p = p;
+	}
+}
+
 
 
 class Parent1
@@ -16,7 +50,7 @@ class Child1 extends Parent1
 	
 	Child1()
 	{
-		/*
+		
 		System.out.println(age);
 		System.out.println(this.age);
 		System.out.println(super.age);
@@ -25,8 +59,29 @@ class Child1 extends Parent1
 		
 		System.out.println(System.identityHashCode(age));
 		System.out.println(System.identityHashCode(this.age));
-		System.out.println(System.identityHashCode(super.age));*/
+		System.out.println(System.identityHashCode(super.age));
+		
+		System.out.println(this);
+		System.out.println(super.toString());
 
+	}
+}
+
+
+class Product
+{
+	int price;
+	Product(int price)
+	{
+		this.price = price;
+	}
+}
+
+class Tv extends Product
+{
+	Tv()
+	{
+		
 	}
 }
 
@@ -37,14 +92,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		Parent1 parent = new Parent1();
-		parent.age = 80;
-		
-		System.out.println(System.identityHashCode(parent.age));
+
 		Child1 child = new Child1();
 		
-		System.out.println(System.identityHashCode(child.age));
+
 	}
 
 	
@@ -94,13 +145,21 @@ public class Main {
 	
 	public static void 실습문제2_1()
 	{
-		ElectricCar car = new ElectricCar();
 		
-		car.battery= 50;
+		/*
+		 * 오버라이딩은 메서드가 반환타입까지 포함하여 완전히 일치해야한다
+		 * 공변반환타입만 예외가 적용된다.
+		 */
 		
-		car.Charge(30);
+	}
+	
+	public static void 실습문제2_2()
+	{
 		
-		System.out.println("현재 배터리량:"+car.battery);
-		
+		/*TV의 부모인 Product의 기본 생성자가 없다. 
+		 * 자식은 무조건 부모의 생성자를 호출하도록되어 있으며
+		 * 자동 호출시 기본 생성자를 호출한다.
+		 * 
+		 */
 	}
 }
